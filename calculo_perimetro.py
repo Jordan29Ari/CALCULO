7ba2bc4b6fc14cdac914729f9ec8727834edb663
Jordan29Ari <ariasbailonjordanjose@gmail.com>
2024-10-24 07:51:22 -0500

    Create calculo_perimetro.py

---
+++ calculo_perimetro.py
@@ -0,0 +1,10 @@
+l1=float(input("Digite el lado 1 de cuadrado: "))
+l2=float(input("Digite el lado 2 de cuadrado: "))
+l3=float(input("Digite el lado 3 de cuadrado: "))
+l4=float(input("Digite el lado 4 de cuadrado: "))
+
+
+area=l1*l2
+perimetro=l1+l2+l3+l4
+print("El area del cuadrado es de: ",area)
+print("El perimetro del cuadrado es de: ",perimetro)
